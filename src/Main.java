2bd97a7947ca1bd7fb8758ff93dd7da8df2a8a71
Alexander <emotionless013@mail.ru>
2022-09-05 20:27:00 +0400

    hw2.7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 12, 23, 4, 4, 5, 5, 6, 7));
+//Задание 1
+        List<Integer> taskOne = new ArrayList<>(1);
+        for (Integer num : nums) {
+            if (num % 2 == 1) {
+                taskOne.add(num);
+            }
+        }
+        System.out.println(taskOne);
+//Задание 2
+        List<Integer> taskTwo = new ArrayList<>(1);
+        for (Integer num : nums) {
+            if (num % 2 == 0 && !taskTwo.contains(num)) {
+                taskTwo.add(num);
+            }
+        }
+        Collections.sort(taskTwo);
+        System.out.println(taskTwo);
+//Задание 3
+        List<String> words = new ArrayList<>(List.of("one", "four", "one", "three", "three", "four", "one"));
+        List<String> taskThree = new ArrayList<>(1);
+        for (String word : words) {
+            if (!taskThree.contains(word)) {
+                taskThree.add(word);
+            }
+        }
+        System.out.println(taskThree);
+//Задание 4
+        List<Integer> taskFour = new ArrayList<>(1);
+        List<String> sorted = new ArrayList<>(1);
+        for (String word : words) {
+            if (!sorted.contains(word)) {
+                sorted.add(word);
+                if (Collections.frequency(words, word) > 1) {
+                    taskFour.add(Collections.frequency(words, word));
+                }
+            }
+        }
+        System.out.println(taskFour);
     }
 }
